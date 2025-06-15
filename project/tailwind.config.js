4abfab3fba542ad42ea124c652b3d22ef2c1d54d
LuciferK47 <149688014+LuciferK47@users.noreply.github.com>
2025-06-15 11:38:51 +0530

    Update tailwind.config.js

--- project/tailwind.config.js
+++ project/tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+    content: ['./*.html', './src/**/*.{js,ts,jsx,tsx,html}'],
+  darkMode: 'class', // ✅ Add this line to enable class-based dark mode
   theme: {
     extend: {},
   },
